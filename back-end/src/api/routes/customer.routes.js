af5ea1d4676a28ad25b08fdf53d1188419c77987
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-16 22:21:54 -0300

    feat: updating customer routes

--- back-end/src/api/routes/customer.routes.js
+++ back-end/src/api/routes/customer.routes.js
@@ -4,6 +4,8 @@ const customerController = require('../controller/customer.controller');
 
 const customerRoute = Router();
 
+customerRoute.get('/', customerController.getAllProducts);
 customerRoute.get('/myaccount/', customerController.getPollenBalance);
+customerRoute.put('/myaccount/', customerController.updatePollenBalance);
 
 module.exports = customerRoute;
