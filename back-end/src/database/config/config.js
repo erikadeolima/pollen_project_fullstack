55ef0aabadd1125ef8fec82b66ceee72fcc4dd29
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-16 21:58:19 -0300

    feat: fixing database config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,15 +1,38 @@
 // src/database/config/config.js
+require('dotenv').config();
 
-const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
+const environment = process.env.NODE_ENV || "test";
+
+const suffix = {
+  prod: "",
+  production: "",
+  dev: "-dev",
+  development: "-dev",
+  test: "-test",
+};
+
+const options = {
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  database:
+    `${process.env.MYSQL_DB_NAME || 'app_PollenPoints_database'}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
 };
 
 module.exports = {
-  development: config,
-  test: config,
-  production: config,
+  development: {
+    ...options,
+  },
+  test: {
+    ...options,
+  },
+  production: {
+    ...options,
+  },
 };
