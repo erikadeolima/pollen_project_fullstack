e41c3e07a6109c1563525230bdd124aceaa9c8fd
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-16 22:00:49 -0300

    feat: creating products and users services

---
+++ back-end/src/api/service/users.service.js
@@ -0,0 +1,30 @@
+const { Op } = require('sequelize');
+const { User } = require('../../database/models');
+const errorGenerate = require('../helper/errorGenerate');
+
+const findUserByName = async (name) => {
+  const user = await User.findOne({ where: { name } });
+  if (!user) {
+    throw errorGenerate(404, 'Not found');
+  }
+  return user.id;
+};
+
+const getPollenBalance = async (id) => {
+  const user = await User.findOne({ where: { id } });
+  if (!user) {
+    throw errorGenerate(404, 'Not found');
+  }
+  return user.pollenBalance;
+};
+
+const updatePollenBalance = async (id, balance) => {
+  const newBallance = await User.update({ pollenBalance: balance }, { where: { id } });
+  console.log(newBallance)
+};
+
+module.exports = {
+  findUserByName,
+  updatePollenBalance,
+  getPollenBalance
+};
